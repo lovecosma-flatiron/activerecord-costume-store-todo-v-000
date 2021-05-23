a62510518a6c72180105fd78232ce9d58a0769f3
Matthew Williams <matthew.williams@flatironschool.com>
2021-05-23 12:02:16 -0600

    commit tree 3118124711969efee178328f4b510cdf4d549ffc

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+    def change 
+        create_table :haunted_houses do |a|
+            a.string :name
+            a.string :location
+            a.string :theme
+            a.float :price
+            a.boolean :family_friendly
+            a.datetime :opening_date
+            a.datetime :closing_date
+            a.text :description
+            a.timestamp :created_at
+            a.timestamp :updated_at
+        end 
+    end 
+end 
